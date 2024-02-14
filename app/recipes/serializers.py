691e9c8ba46bde1fab7fd90759efa24924763914
Rabie <rabiezahid@gmail.com>
2024-02-14 13:44:09 +1100

    Implement create recipe api

--- app/recipes/serializers.py
+++ app/recipes/serializers.py
@@ -13,12 +13,6 @@ class RecipeSerializer(serializers.ModelSerializer):
         fields = ['id', 'title', 'time_minutes', 'price', 'link']
         read_only_fields = ['id']
 
-    def create(self, validated_data):
-        """Create and return recipe."""
-        auth_user = self.context['request'].user
-        recipe = Recipe.objects.create(user=auth_user, **validated_data)
-        return recipe
-
 
 class RecipeDetailSerializer(RecipeSerializer):
     """Serializer for recipe detail view."""
--- app/recipes/views.py
+++ app/recipes/views.py
@@ -26,3 +26,7 @@ class RecipeViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         """Retrieve recipes for authenticated users."""
         return self.queryset.filter(user=self.request.user).order_by('-id')
+
+    def perform_create(self, serializer):
+        """Create a new recipe."""
+        serializer.save(user=self.request.user)
